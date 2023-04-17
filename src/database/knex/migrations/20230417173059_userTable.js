e95c7d93cd3124416ab9911b0fe76958aec1af30
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-17 14:57:28 -0300

    update database

--- src/database/knex/migrations/20230417173059_userTable.js
+++ src/database/knex/migrations/20230417173059_userTable.js
@@ -1,4 +1,4 @@
-exports.up = knex => knex.schema.createtable("user", table => {
+exports.up = knex => knex.schema.createTable("user", table => {
   table.increments("id");
   table.text("name");
   table.text("email");
