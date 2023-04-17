0c4d2dc842cfcb4598003be2be53c3f6265c6c7e
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-17 15:00:57 -0300

    update database

--- src/database/knex/migrations/20230417173136_favoritesTable.js
+++ src/database/knex/migrations/20230417173136_favoritesTable.js
@@ -2,7 +2,6 @@ exports.up = knex => knex.schema.createTable("favorites", table => {
   table.increments("id");
   table.integer("user_id").references("id").inTable("user");
   table.integer("dishes_id").references("id").inTable("dishes");
-  table.text("name");
 });
 
 exports.down = knex => knex.schema.dropTable("favorites");
