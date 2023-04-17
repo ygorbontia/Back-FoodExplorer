a50fe78690cfedaf020f0d76aa68b8181ca81009
Ygor Bontia <ygorbontiaa@gmail.com>
2023-04-17 19:40:06 -0300

    update dishes method

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,3 +1,4 @@
+const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
 
 class DishesController {
@@ -19,6 +20,41 @@ class DishesController {
         name: ingredient
       }
     });
+
+    await knex("ingredients").insert(ingredientsInsert);
+
+    return res.json();
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { name, price, description, category, ingredients } = req.body;
+
+    const dish = await knex("dishes").where({ id }).first();
+    if (!dish) {
+      throw new AppError("O prato não foi encontrado.");
+    }
+    
+    dish.name = name ?? dish.name;
+    dish.name = price ?? dish.price;
+    dish.name = description ?? dish.description;
+    dish.name = category ?? dish.category;
+
+    await knex("dishes").where({ id }).update({
+      name: dish.name,
+      price: dish.price,
+      description: dish.description,
+      category: dish.category
+    })
+
+    const ingredientsInsert = ingredients.map(ingredient => {
+      return {
+        dishes_id: id,
+        name: ingredient
+      }
+    })
+
+    await knex("ingredients").where("dishes_id", id).delete();
 
     await knex("ingredients").insert(ingredientsInsert);
 
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -5,5 +5,6 @@ const DishesController = require("../controllers/DishesController");
 const dishesController = new DishesController();
 
 dishesRoutes.post("/:user_id", dishesController.create);
+dishesRoutes.put("/:id", dishesController.update);
 
 module.exports = dishesRoutes;
